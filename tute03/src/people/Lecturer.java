cd061a528a20624df71858c99ad557ad7b75c299
Jeffrey Chu <jeffrey.chu1@student.unsw.edu.au>
2026-06-16 16:28:03 +1000

    Tutorial 3 content

---
+++ tute03/src/people/Lecturer.java
@@ -0,0 +1,33 @@
+package people;
+
+import java.util.Map;
+
+public class Lecturer extends Person {
+    public static final Map<String, Integer> PAY_RATES = Map.of(
+        "LVL0", 0,
+        "LVL1", 1000,
+        "LVL2", 2000,
+        "LVL3", 3000
+    );
+
+    private int salary;
+    public Lecturer(String name, int age, String payRate) {
+        super(name, age);
+        setSalary(payRate);
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    /**
+     * Sets the salary of a person given their pay rate
+     * @param payRate New pay rate of the person
+     * @precondition payRate is a key in PAY_RATES
+     * @postcondition salary is the value in PAY_RATES
+     */
+    public void setSalary(String payRate) {
+        Integer pay = PAY_RATES.get(payRate);
+        salary = pay;
+    }
+}
---
+++ tute03/src/wondrous/Wondrous.java
@@ -0,0 +1,32 @@
+package wondrous;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Wondrous {
+    private final int MY_MAGIC_NUMBER = 42;
+
+    public List<Integer> wondrous(int start) {
+        int current = start;
+        List<Integer> sequence = new ArrayList<Integer>();
+        if (current < 1) {
+            throw new IllegalArgumentException("Cannot have a start less than 1");
+        }
+        if (current == 1) {
+            return new ArrayList<>();
+        }
+
+        while (current != 1) {
+            sequence.add(current);
+            if (current % 2 == 0) {
+                current /= 2;
+            } else {
+                current = (current * 3) + 1;
+            }
+        }
+
+        sequence.add(current);
+
+        return sequence;
+    }
+}
